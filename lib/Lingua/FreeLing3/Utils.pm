708bf2ee5fad2de22a71a168690eea912265ad09
Nuno Carvalho <mestre.smash@gmail.com>
2012-12-19 22:33:54 +0000

    Import of SMASH/Lingua-FreeLing3-Utils-0.02 from CPAN.
    gitpan-cpan-distribution: Lingua-FreeLing3-Utils
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SMASH/Lingua-FreeLing3-Utils-0.02.tar.gz
    gitpan-cpan-author:       SMASH
    gitpan-cpan-maturity:     released

--- lib/Lingua/FreeLing3/Utils.pm
+++ lib/Lingua/FreeLing3/Utils.pm
@@ -19,11 +19,11 @@ Lingua::FreeLing3::Utils - text processing utilities using FreeLing3 Perl inferf
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
